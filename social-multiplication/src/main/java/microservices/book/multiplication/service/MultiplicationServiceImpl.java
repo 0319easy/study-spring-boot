114b30a5a92d94024d801a2464870e2791ba10b2
0319easy <0319easy@gmail.com>
2020-12-04 01:46:44 +0900

    chapter02 TDD로 간단한 스프링 부트 애플리케이션 작성

---
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
@@ -0,0 +1,23 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MultiplicationServiceImpl implements MultiplicationService {
+
+    private RandomGeneratorService randomGeneratorService;
+
+    @Autowired  // TODO @Autowired가 생성자에 붙어 있으면 무슨 의미인지 찾아보기
+    public MultiplicationServiceImpl(RandomGeneratorService randomGeneratorService) {
+        this.randomGeneratorService = randomGeneratorService;
+    }
+
+    @Override
+    public Multiplication createRandomMultiplication() {
+        int factorA = randomGeneratorService.generateRandomFactor();
+        int factorB = randomGeneratorService.generateRandomFactor();
+        return new Multiplication(factorA, factorB);
+    }
+}
